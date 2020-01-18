f9f803889087937585475e631de008d8332b2610
Himanshu Kandpal <cse10324.sbit@gmail.com>
2020-01-18 16:06:20 +0530

    implemented @PostContruct for preloading data

--- spring-rest-demo/src/main/java/com/himanshu/springdemo/restService/StudentService.java
+++ spring-rest-demo/src/main/java/com/himanshu/springdemo/restService/StudentService.java
@@ -3,7 +3,10 @@ package com.himanshu.springdemo.restService;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +16,25 @@ import com.himanshu.springdemo.config.Student;
 @RequestMapping("/api")
 public class StudentService {
 
-//	student mapping to get list of students
-	@GetMapping("/students")
-	public List<Student> listStudents(){
-		List<Student> theStudent = new ArrayList<Student>();
-		
+	private List<Student> theStudent;	
+	
+	@PostConstruct
+	public void loadData() {
+		theStudent = new ArrayList<>();
 		theStudent.add(new Student("R","Kandpal"));
 		theStudent.add(new Student("D","Joshi"));
 		theStudent.add(new Student("H","Kandpal"));
-		
+	}
+	
+//	student mapping to get list of students
+	@GetMapping("/students")
+	public List<Student> listStudents(){
 		return theStudent;
-		
+	}
+	
+//	define endpoint for /student/{studentID}
+	@GetMapping("/students/{studentId}")
+	public Student loadStudentId(@PathVariable int studentId){
+		return theStudent.get(studentId);
 	}
 }
